9ed2fe766c6944487054fa3c253a8f4c3cd046b6
Byron Kamal <byronkamal@hotmail.com>
2018-05-15 11:20:33 -0300

    Atualizando trabalho 3

--- trabalho3.c
+++ trabalho3.c
@@ -16,10 +16,33 @@ Agenda* iniciaVazio(void) {
 	return NULL;
 }
 
+Agenda* insereRegistro(Agenda* atual, char *nome, char *celular, char *endereco, unsigned int cep, char *dataNasc){
+	Agenda* novo = (Agenda*) malloc(sizeof(Agenda));
+	
 
+	
+	novo->proximo = atual;
+	novo->anterior = NULL;
+	
+	if(atual != NULL) {
+		atual->anterior = novo;
+	}
+	
+	return novo;
+}
 
-int main () {
+int main() {
 
-
+	Agenda* registro = (Agenda*) malloc(sizeof(Agenda));
+	registro->cep = 555;
+	registro->proximo = NULL;
+	registro->anterior = NULL;
+		
+	gets(registro->nome);
+	gets( registro->celular);
+	gets(registro->endereco);
+	gets(registro->dataNascimento);
+	
+	printf("%s %s %s %s", registro->nome, registro->celular, registro->endereco, registro->dataNascimento);
 return 0;	
 }
