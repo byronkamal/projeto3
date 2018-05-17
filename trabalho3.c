063ca975d1db400a113d6252fa140c682addbf1f
byronkamal <byronkamal@hotmail.com>
2018-05-17 08:49:24 -0300

    Atualizando

--- trabalho3.c
+++ trabalho3.c
@@ -1,48 +1,236 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct agenda {
-char nome[100];
-char celular[9];
-char endereco[100];
-unsigned int cep;
-char dataNascimento[10]; 
 
-struct agenda* anterior;
-struct agenda* proximo;
-}Agenda;
+typedef struct Registro{
+  char nomeCompleto[100];
+  char celular[10];
+  char endereco[100];
+  unsigned int CEP;
+  char dataNascimento[10];
+  struct Registro *anterior;
+  struct Registro *seguinte;
+}registro;
 
-Agenda* iniciaVazio(void) {
-	return NULL;
+registro *alocaNovoContato();
+registro *inserirNovoRegistro();
+registro *removerContato();
+void visualizarRegistro();
+void visualizarTodosRegistros();
+registro *inicializarAgenda();
+void salvarAgenda();
+void freeAll();
+
+int main(){
+  FILE *ptr = NULL;
+  registro *head;
+  int resposta;
+
+  head = inicializarAgenda(ptr);
+
+  do{
+
+    printf("Opcao 0 - Sair\n");
+    printf("Opcao 1 - Inserir novo nome\n");
+    printf("Opcao 2 - Remover contato\n");
+    printf("Opcao 3 - Visualizar registro\n");
+    printf("Opcao 4 - Visualizar todos os registros\n");
+
+    scanf("%d", &resposta);
+    switch (resposta) {
+      case 0:
+        salvarAgenda(head);
+      break;
+      case 1:
+        head = inserirNovoRegistro(head);
+      break;
+      case 2:
+        head = removerContato(head);
+      break;
+      case 3:
+        visualizarRegistro(head);
+      break;
+      case 4:
+        visualizarTodosRegistros(head);
+      break;
+      default:
+        printf("Entre com um valor entre 0 e 4\n");
+    }
+  }while(resposta);
+
+  fclose(ptr);
+  freeAll(head);
+  return 0;
 }
 
-Agenda* insereRegistro(Agenda* atual, char *nome, char *celular, char *endereco, unsigned int cep, char *dataNasc){
-	Agenda* novo = (Agenda*) malloc(sizeof(Agenda));
-	
-
-	
-	novo->proximo = atual;
-	novo->anterior = NULL;
-	
-	if(atual != NULL) {
-		atual->anterior = novo;
-	}
-	
-	return novo;
+registro *alocaNovoContado(){
+  return malloc(sizeof(registro));
 }
 
-int main() {
+registro *inserirNovoRegistro(registro *head){
+  registro *corrente;
+  registro *antes;
+  registro *novoRegistro = alocaNovoContado();
 
-	Agenda* registro = (Agenda*) malloc(sizeof(Agenda));
-	registro->cep = 555;
-	registro->proximo = NULL;
-	registro->anterior = NULL;
-		
-	gets(registro->nome);
-	gets( registro->celular);
-	gets(registro->endereco);
-	gets(registro->dataNascimento);
-	
-	printf("%s %s %s %s", registro->nome, registro->celular, registro->endereco, registro->dataNascimento);
-return 0;	
+  scanf("%s", novoRegistro->nomeCompleto);
+  scanf("%s", novoRegistro->celular);
+  scanf("%s", novoRegistro->endereco);
+  scanf("%s", novoRegistro->dataNascimento);
+  scanf("%d", &novoRegistro->CEP);
+
+  printf("registrado\n");
+
+  if(head == NULL){
+    head = novoRegistro;
+  }
+  else{
+    corrente = head;
+    while(corrente != NULL){
+      if(strcmp(novoRegistro->nomeCompleto, corrente->nomeCompleto) > 0 && corrente->seguinte != NULL){
+        corrente = corrente->seguinte;
+      }
+      else if(strcmp(novoRegistro->nomeCompleto, corrente->nomeCompleto) > 0 && corrente->seguinte == NULL){
+        corrente->seguinte = novoRegistro;
+        //antes->seguinte = novoRegistro;
+        novoRegistro->anterior = corrente;
+        novoRegistro->seguinte = NULL;
+        break;
+      }
+      else if(strcmp(novoRegistro->nomeCompleto, corrente->nomeCompleto) < 0){
+        if(corrente->anterior == NULL){
+          head = novoRegistro;
+          novoRegistro->anterior = NULL;
+          novoRegistro->seguinte = corrente;
+          corrente->anterior = novoRegistro;
+        }
+        else{
+          antes = corrente->anterior;
+          antes->seguinte = novoRegistro;
+          novoRegistro->seguinte = corrente;
+          novoRegistro->anterior = antes;
+          corrente->anterior = novoRegistro;
+        }
+        break;
+      }
+    }
+  }
+  //printf("%s\n", head->nomeCompleto)
+
+  return head;
+}
+
+registro *inicializarAgenda(FILE *ptr){
+  registro *corrente;
+  registro *head;
+  registro *antes;
+  char lixo[2];
+
+  int primeiroRegistro = 1;
+
+  if((ptr = fopen("contatos.txt", "r")) == NULL){
+    return NULL;
+  }
+
+  while(!feof(ptr)){
+    corrente = alocaNovoContado();
+    fscanf(ptr, "%s\n%s\n%s\n%d\n%s\n%s\n", corrente->nomeCompleto, corrente->celular, corrente->endereco, &corrente->CEP, corrente->dataNascimento,lixo);
+    //fscanf(ptr,"%s", &lixo);
+    if(primeiroRegistro){
+      head = corrente;
+      antes = head;
+      primeiroRegistro = 0;
+    }
+    else{
+      antes->seguinte = corrente;
+      corrente->anterior = antes;
+      antes = corrente;
+    }
+
+  }
+  return head;
+}
+
+void salvarAgenda(registro *head){
+  registro *corrente = head;
+  FILE *ptr = NULL;
+
+  if(head == NULL)
+    return;
+
+  if((ptr = fopen("contatos.txt", "w")) == NULL){
+    printf("Nao foi possivel abrir o aquivo.\n");
+  }
+  else{
+    while(corrente != NULL){
+      //printf("%15s %14p %14p %14p\n", corrente->nomeCompleto, corrente->anterior, corrente, corrente->seguinte);
+      fprintf(ptr, "%s\n%s\n%s\n%d\n%s\n$\n", corrente->nomeCompleto, corrente->celular, corrente->endereco, corrente->CEP, corrente->dataNascimento);
+      corrente = corrente->seguinte;
+    }
+  }
+}
+
+void freeAll(registro *head){
+  registro *aux;
+  registro *corrente = head;
+  while(corrente->seguinte != NULL){
+    aux = corrente;
+    corrente = corrente->seguinte;
+    free(aux);
+  }
+}
+
+void visualizarTodosRegistros(registro *head){
+
+/* imprime primeiro elemento */
+printf("%s\n", head->nomeCompleto);
+      printf("%s\n", head->celular);
+      printf("%s\n", head->endereco);
+      printf("%d\n", head->CEP);
+      printf("%s\n", head->dataNascimento);
+
+/* imprime sub-lista */
+visualizarTodosRegistros(head->seguinte);
+
+}
+
+void visualizarRegistro(registro *head){
+  char nome[100];
+  registro *corrente = head;
+  scanf("%s", nome);
+
+  while(corrente != NULL){
+    if(strcmp(corrente->nomeCompleto, nome) == 0){
+      printf("%s\n", corrente->nomeCompleto);
+      printf("%s\n", corrente->celular);
+      printf("%s\n", corrente->endereco);
+      printf("%d\n", corrente->CEP);
+      printf("%s\n", corrente->dataNascimento);
+      return;
+    }
+    corrente = corrente->seguinte;
+  }
+
+  printf("Nome nao encontrado!\n");
+}
+
+registro *removerContato(registro *head){
+  char nome[100];
+  registro *corrente = head;
+  registro *antes, *depois;
+  scanf("%s", nome);
+
+  while(corrente != NULL){
+    antes = corrente->anterior;
+    depois = corrente->seguinte;
+    if(strcmp(corrente->nomeCompleto, nome) == 0){
+      antes->seguinte = depois;
+      depois->anterior = antes;
+      return head;
+    }
+    corrente = corrente->seguinte;
+  }
+
+  printf("Nome nao encontrado!\n");
+  return head;
 }
